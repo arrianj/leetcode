68487714c270f7b4503af5e9a92e4a83ea112cbb
Arrian Jahangiri <arrianj@gmail.com>
2021-02-12 22:21:14 -0600

    solved 1221

---
+++ 1221.py
@@ -0,0 +1,13 @@
+def test(string: str):
+    count = 0
+    splits = 0
+    for i in string:
+        if i == 'L':
+            count += 1
+        if i == 'R':
+            count -= 1
+        if count == 0:
+            splits += 1
+    return splits
+
+# runtime 32 ms, 14.2 MB memory
