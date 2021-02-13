1e6e71e8b88dd942cf187fb18188810b40129a5a
Arrian Jahangiri <arrianj@gmail.com>
2021-02-12 19:07:20 -0600

    updated to show results using AREPL extension

--- 1480.py
+++ 1480.py
@@ -1,11 +1,12 @@
-class Solution:
-    def runningSum(self, nums: List[int]) -> List[int]:
-        ans = []
-        for i in nums:
-            if ans == []:
-                ans.append(i)
-            else:
-                ans.append(i + ans[-1])
-        return ans
+def test(nums):
+    ans = []
+    for i in nums:
+        if ans == []:
+            ans.append(i)
+        else:
+            ans.append(i + ans[-1])
+    return ans
+
+print(test([2,3,0,2]))
 
 # 28 ms runtime, 14.4 MB memory
