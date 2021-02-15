3c3994203f736849fbab275eb309fdbcd72064eb
Arrian Jahangiri <arrianj@gmail.com>
2021-02-15 01:51:04 -0600

    late night easies

---
+++ 1051.py
@@ -0,0 +1,13 @@
+def test(heights: list):
+    moved = 0
+
+    sortedlist = sorted(heights)
+
+    for idx, i in enumerate(heights):
+        if i != sortedlist[idx]:
+            moved += 1
+    return moved
+
+print(test([5,1,2,3,4]))
+
+# runtime 32 ms, 14.2 MB memory
---
+++ 657.py
@@ -0,0 +1,25 @@
+def test(moves: str):
+    start_x = 0
+    start_y = 0
+    end_x = 0
+    end_y = 0
+
+    for i in moves:
+        if i == 'L':
+            end_x -= 1
+        if i == 'R':
+            end_x += 1
+        if i == 'U':
+            end_y += 1
+        if i == 'D':
+            end_y -= 1
+
+    if end_x == start_x and end_y == start_y:
+        return True
+    else:
+        return False
+
+print(test('UDLRUDLR'))
+
+# alternatively, compare number of ups to downs & lefts to rights. if both comparisons are equal, ans = true
+# runtime 72 ms, 14.4 MB memory
